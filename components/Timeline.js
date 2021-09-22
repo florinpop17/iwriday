d13f61d23406025e98151c06068df0985f9541ec
Florin Pop <popflorin1705@yahoo.com>
2021-09-22 17:25:10 +0300

    added infinite scrolling by Mihai the cool guy

--- components/Timeline.js
+++ components/Timeline.js
@@ -1,34 +1,64 @@
 import { useEffect, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { supabase } from "../lib/supabase";
 
 import Thread from "./Thread";
 
 const Timeline = () => {
     const [threads, setThreads] = useState([]);
+    const [count, setCount] = useState(0);
+
+    const hasMore = threads.length < count - 1;
+
+    // Parameter to set how many items you want to add on scroll
+    const numberOfThreads = 10;
 
     useEffect(() => {
-        fetchThreads();
+        fetchThreads(0, numberOfThreads - 1);
     }, []);
 
-    const fetchThreads = async () => {
-        let { data: threads, error } = await supabase
+    const fetchThreads = async (minRange, maxRange) => {
+        if (maxRange > count) {
+            maxRange = count;
+        }
+
+        let {
+            data: newThreads,
+            error,
+            count,
+        } = await supabase
             .from("posts")
             .select(
-                "id, full_text, date, url, word_count, public_users ( username ) "
+                "id, full_text, date, url, word_count, public_users ( username ) ",
+                { count: "exact" }
             )
+            .range(minRange, maxRange)
             .order("date", { ascending: false });
 
         if (error) console.log("error", error);
         else {
-            setThreads(threads);
+            setThreads((prevThreads) => [...prevThreads, ...newThreads]);
+            setCount(count);
         }
     };
 
     return (
         <div>
-            {threads.map((thread) => (
-                <Thread key={thread.id} thread={thread} />
-            ))}
+            <InfiniteScroll
+                dataLength={threads.length}
+                next={() =>
+                    fetchThreads(
+                        threads.length + 1,
+                        threads.length + numberOfThreads
+                    )
+                }
+                hasMore={hasMore}
+                loader={<h4 className="text-2xl">Loading...</h4>}
+            >
+                {threads.map((thread) => (
+                    <Thread key={thread.id} thread={thread} />
+                ))}
+            </InfiniteScroll>
         </div>
     );
 };
