0bacd947275d430050722542b2e76e1f88c599f8
Florin Pop <popflorin1705@yahoo.com>
2021-09-22 19:15:51 +0300

    updated keys

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const Twit = require("twit");
 const { createClient } = require("@supabase/supabase-js");
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_SECRET_KEY;
+const SUPABASE_KEY = process.env.SUPABASE_SECRET_KEY;
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const T = new Twit({
--- streak-checker.js
+++ streak-checker.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { createClient } = require("@supabase/supabase-js");
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_SECRET_KEY;
+const SUPABASE_KEY = process.env.SUPABASE_SECRET_KEY;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
