0a3fc31099c568c7149a9cba9a38d441e9e613c8
Florin Pop <popflorin1705@yahoo.com>
2021-10-15 17:47:02 +0300

    added video, remove apis

--- pages/how-it-works.js
+++ pages/how-it-works.js
@@ -1,7 +1,22 @@
 import { Layout } from "../components/";
 
 const HowPage = () => {
-    return <Layout showSidebar={false}>We will have a video here.</Layout>;
+    return (
+        <Layout showSidebar={false}>
+            <div class="relative" style={{ paddingTop: "56.25%" }}>
+                <iframe
+                    className="absolute inset-0 w-full h-full"
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/mn6OgF70lNY"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                ></iframe>
+            </div>
+        </Layout>
+    );
 };
 
 export default HowPage;
