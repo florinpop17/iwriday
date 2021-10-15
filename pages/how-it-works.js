d40d57a672681ed013babb295df259741caf28ed
Florin Pop <popflorin1705@yahoo.com>
2021-10-15 17:49:17 +0300

    fixed typos

--- pages/how-it-works.js
+++ pages/how-it-works.js
@@ -3,16 +3,16 @@ import { Layout } from "../components/";
 const HowPage = () => {
     return (
         <Layout showSidebar={false}>
-            <div class="relative" style={{ paddingTop: "56.25%" }}>
+            <div className="relative" style={{ paddingTop: "56.25%" }}>
                 <iframe
                     className="absolute inset-0 w-full h-full"
                     width="560"
                     height="315"
                     src="https://www.youtube.com/embed/mn6OgF70lNY"
                     title="YouTube video player"
-                    frameborder="0"
+                    frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen
+                    allowFullScreen
                 ></iframe>
             </div>
         </Layout>
